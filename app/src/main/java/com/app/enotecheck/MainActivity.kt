a9b083dcbc9013541466b815353e81afbc652384
Nizar <ettaheri.nizar@gmail.com>
2025-03-10 23:08:35 +0000

    refacto : add button to open enote web site

--- app/src/main/java/com/app/enotecheck/MainActivity.kt
+++ app/src/main/java/com/app/enotecheck/MainActivity.kt
@@ -1,118 +1,116 @@
 package com.app.enotecheck
 
-import android.graphics.Color
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.animation.AnimationUtils
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import androidx.core.view.WindowInsetsControllerCompat
 import com.airbnb.lottie.LottieAnimationView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okio.IOException
+import okhttp3.Response
+import java.io.IOException
 import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var button: LottieAnimationView
-    private lateinit var animation: LottieAnimationView
-    private lateinit var resultLayout: LinearLayout
-    private lateinit var imgResult: ImageView
-    private lateinit var textResult: TextView
-
+    private val button by lazy { findViewById<LottieAnimationView>(R.id.lottieButton) }
+    private val animation by lazy { findViewById<LottieAnimationView>(R.id.lottieAnimationView) }
+    private val resultLayout by lazy { findViewById<LinearLayout>(R.id.result_layout) }
+    private val imgResult by lazy { findViewById<ImageView>(R.id.img_result) }
+    private val textResult by lazy { findViewById<TextView>(R.id.text_result) }
+    private val buttonOpen by lazy { findViewById<Button>(R.id.button_open) }
+    private val url = "https://enote.ofppt.ma/"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initViews()
         setupStatusBar()
-
-        button.setOnClickListener {
-            startProcess()
+        button.setOnClickListener { startProcess() }
+        buttonOpen.setOnClickListener {
+            openWebsite(url)
         }
-    }
-
-    private fun initViews() {
-        button = findViewById(R.id.lottieButton)
-        animation = findViewById(R.id.lottieAnimationView)
-        resultLayout = findViewById(R.id.result_layout)
-        imgResult = findViewById(R.id.img_result)
-        textResult = findViewById(R.id.text_result)
     }
 
     private fun setupStatusBar() {
         val windowInsetsController = WindowInsetsControllerCompat(window, window.decorView)
         windowInsetsController.isAppearanceLightStatusBars = true
-        window.statusBarColor = resources.getColor(R.color.white, null)
+        window.statusBarColor = getColor(R.color.white)
     }
 
     private fun startProcess() {
-        button.visibility = View.GONE
-        resultLayout.visibility = View.GONE
-        animation.visibility = View.VISIBLE
+        updateUI(isLoading = true)
 
         CoroutineScope(Dispatchers.IO).launch {
             val client = OkHttpClient()
-            val request = Request.Builder().url("https://enote.ofppt.ma/").build()
+            val request = Request.Builder().url(url).build()
 
             try {
                 val responseTime = measureTimeMillis {
                     client.newCall(request).execute().use { response ->
-                        handleResponse(response)
+                        processResponse(response)
                     }
                 }
-                println("Response Time: ${responseTime}ms")
+                Log.d("MainActivity", "Response Time: ${responseTime}ms")
             } catch (e: IOException) {
-                handleError(e)
+                showError(e)
             }
         }
     }
 
-    private suspend fun handleResponse(response: okhttp3.Response) {
+    private suspend fun processResponse(response: Response) {
         withContext(Dispatchers.Main) {
-            when {
-                response.isSuccessful -> {
-                    showResult(R.drawable.verifier, R.string.result_ok, R.color.green)
-                }
-                else -> {
-                    showResult(R.drawable.attention, R.string.result_intrernal_issue, R.color.yellow)
-                }
+            if (response.isSuccessful) {
+                showResult(R.drawable.verifier, R.string.result_ok, R.color.green, showOpenButton = true)
+            } else {
+                showResult(R.drawable.attention, R.string.result_internal_issue, R.color.yellow, showOpenButton = false)
             }
-            button.visibility = View.VISIBLE
-            animation.visibility = View.GONE
-            resultLayout.visibility = View.VISIBLE
         }
     }
 
-    private suspend fun handleError(e: IOException) {
+    private suspend fun showError(e: IOException) {
+        Log.e("MainActivity", "Website is DOWN! Error: ${e.message}")
         withContext(Dispatchers.Main) {
-            showResult(R.drawable.erreur, R.string.result_down, R.color.red)
-            println("Website is DOWN! Error: ${e.message}")
-            button.visibility = View.VISIBLE
-            animation.visibility = View.GONE
-            resultLayout.visibility = View.VISIBLE
+            showResult(R.drawable.erreur, R.string.result_down, R.color.red, showOpenButton = false)
         }
     }
 
-    private fun showResult(imageResId: Int, textResId: Int, textColorResId: Int) {
-        Glide.with(this@MainActivity)
+    private fun showResult(imageResId: Int, textResId: Int, textColorResId: Int, showOpenButton: Boolean) {
+        Glide.with(this)
             .load(imageResId)
             .transition(DrawableTransitionOptions.withCrossFade())
             .into(imgResult)
 
-        textResult.setTextColor(getColor(textColorResId))
-        textResult.setText(textResId)
+        textResult.apply {
+            setText(textResId)
+            setTextColor(getColor(textColorResId))
+            startAnimation(AnimationUtils.loadAnimation(this@MainActivity, R.anim.fade_in))
+        }
 
-        val fadeIn = AnimationUtils.loadAnimation(this@MainActivity, R.anim.fade_in)
-        textResult.startAnimation(fadeIn)
+        buttonOpen.visibility = if (showOpenButton) View.VISIBLE else View.GONE
+        buttonOpen.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in))
+
+        updateUI(isLoading = false)
     }
+
+    private fun updateUI(isLoading: Boolean) {
+        button.visibility = if (isLoading) View.GONE else View.VISIBLE
+        animation.visibility = if (isLoading) View.VISIBLE else View.GONE
+        resultLayout.visibility = if (isLoading) View.GONE else View.VISIBLE
+    }
+    private fun openWebsite(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
+    }
+
 }
